51a43846eb994d3110e40857d7597561c0a43bf5
Michel Machado <michelmachado125@gmail.com>
2024-03-08 11:55:52 -0300

    fluxo buscar informações do candidato

---
+++ src/main/java/com/grupo04pj01/urna/interfaceeleitor/services/impl/CandidatoServiceImpl.java
@@ -0,0 +1,48 @@
+package com.grupo04pj01.urna.interfaceeleitor.services.impl;
+
+import com.grupo04pj01.urna.interfaceeleitor.DTO.BuscaCandidatoDTO;
+import com.grupo04pj01.urna.interfaceeleitor.DTO.CandidatoDTO;
+import com.grupo04pj01.urna.interfaceeleitor.DTO.EnvioVotoDTO;
+import com.grupo04pj01.urna.interfaceeleitor.DTO.ResponseLiberacaoUrnaDTO;
+import com.grupo04pj01.urna.interfaceeleitor.services.CandidatoService;
+import com.grupo04pj01.urna.interfaceeleitor.services.VotarService;
+import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class CandidatoServiceImpl implements CandidatoService {
+
+
+    private final RestTemplate restTemplate;
+
+    @Override
+    public Boolean verificarStatusUrna() {
+        ResponseEntity<ResponseLiberacaoUrnaDTO> resposta= restTemplate.getForEntity("http://localhost:8080/liberacao_urna",ResponseLiberacaoUrnaDTO.class);
+        ResponseLiberacaoUrnaDTO bodyResposta= resposta.getBody();
+        return bodyResposta.getIsUrnaLiberada();
+    }
+
+    @Override
+    public CandidatoDTO findCandidato(BuscaCandidatoDTO buscaCandidatoDTO) {
+
+        JSONObject body= new JSONObject();
+        body.put("chapa",buscaCandidatoDTO.getChapa());
+
+        HttpHeaders httpHeaders= new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> requestEntity = new HttpEntity<String>(body.toString(),httpHeaders);
+
+        ResponseEntity<CandidatoDTO> busca= restTemplate.postForEntity("http://localhost:8080/candidato/busca", requestEntity, CandidatoDTO.class);
+
+        return busca.getBody();
+    }
+
+
+}
